0d78ac98c7c1332e5d5f4f7abaf2ca947c3f2fe3
ryan farber <32393930+ryanfarber@users.noreply.github.com>
2020-08-15 22:35:50 -0700

    adjusted message

--- contexts/transmission-context.js
+++ contexts/transmission-context.js
@@ -1,11 +1,11 @@
 // transmission-context.js
 
 function TransmissionContext(input = {}) {
-	this.name = "transmission"
-	this.timestamp = input.timestamp
-	this.torrentName = input.name
-	this.directory = input.directory
-	this.message = "Torrent downloaded!"
+	this.name = "transmission";
+	this.timestamp = input.timestamp;
+	this.torrentName = input.name;
+	this.directory = input.directory;
+	this.message = (this.torrentName) ? "Torrent downloaded!" : "Test message";
 };
 
 module.exports = TransmissionContext
