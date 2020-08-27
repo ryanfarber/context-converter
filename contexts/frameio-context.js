f011baf6cb534b4c5b391f7dba2094827bce8a62
ryan farber <32393930+ryanfarber@users.noreply.github.com>
2020-08-26 17:27:52 -0700

    updated version

--- contexts/frameio-context.js
+++ contexts/frameio-context.js
@@ -1,10 +1,10 @@
 // frame.io context
 
-function FrameioContext(input, settings) {
+function FrameioContext(input = { resource: {}, project: {}, user: {} }, settings = {}) {
 
-    if (this.settings.debug) console.log(input)
+    if (settings.debug) console.log(input)
 
-    this.name = 'frameio'
+    this.name = "frameio"
     this.action = input.type
     this.project = input.project.id
     this.resource = {
@@ -14,27 +14,28 @@ function FrameioContext(input, settings) {
     this.user = {
       id: input.user.id
     }
+    this.message = undefined
     
     switch (this.action) {
-      case 'comment.created':
-        this.message = 'new comment'
+      case undefined:
+        break;
+      case "comment.created":
+        this.message = "new comment"
         break
-      case 'reviewlink.created':
-        this.message = 'new review link created'
+      case "reviewlink.created":
+        this.message = "new review link created"
         break
-      case 'asset.created':
-        this.message = 'asset added.  transcoding...'
+      case "asset.created":
+        this.message = "asset added.  transcoding..."
         break
-      case 'asset.ready':
-        this.message = 'transcoding finished! asset is ready'
+      case "asset.ready":
+        this.message = "transcoding finished! asset is ready"
         break
       default:
-        console.log('new activity')
+        console.log("new activity")
         break
-        // this.message = 'new activity'
     };
     
-    // return this
   }; // END FrameioContext
 
   module.exports = FrameioContext
--- test.js
+++ test.js
@@ -174,11 +174,13 @@ var twitch = new ContextBuilder.TwitchContext()
 var discord = new ContextBuilder.DiscordContext()
 var twilio = new ContextBuilder.TwilioContext()
 var github = new ContextBuilder.GithubContext(githubData)
+var frameio = new ContextBuilder.FrameioContext()
 
 // console.log(twitch)
 // console.log(discord)
 // console.log(twilio)
-console.log(github)
+// console.log(github)
+console.log(frameio)
 
 
 
