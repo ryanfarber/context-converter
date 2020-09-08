4cb329657accf60e32dd161864e39c1653d060fb
ryan farber <32393930+ryanfarber@users.noreply.github.com>
2020-09-07 17:40:19 -0700

    update

--- contexts/ccc-context.js
+++ contexts/ccc-context.js
@@ -1,28 +1,32 @@
 // ccc-context.js
 
-function CCCContext(input, settings = {}) {
+function CCCContext(input = undefined, settings = {}) {
 
-	input = input.split('|')
-	
-	this.task = input[0]
-	this.copyfrom = input[1]
-	this.copyto = input[2]
-	this.date = input[3]
-	this.time = input[4]
-	this.datacopied = input[5]
-	this.status = input[6]
-	this.exitcode = input[7]
+	if (input == undefined) {
+		return
+	} else {
+		input = input.result.split("|")
 
-	this.path = `\`${this.copyfrom} > ${this.copyto}\``;
-	this.copyfromformat = `\`${this.copyfrom}\``;
-	this.copytoformat = `\`${this.copyto}\``;
+		this.task = input[0]
+		this.copyfrom = input[1]
+		this.copyto = input[2]
+		this.date = input[3]
+		this.time = input[4]
+		this.datacopied = input[5]
+		this.status = input[6]
+		this.exitcode = input[7]
 
-	if (this.status == 'Success') {
-		this.message = `**Copy Successful** ✅\n**task**: ${this.task}\n**A**: ${this.copyfromformat}\n**B**: ${this.copytoformat}\n**time elapsed**: ${this.time}\n**data copied**: ${this.datacopied}`;
-	} else 
-	if (this.status == 'Cancelled') {
-		this.message = `**Copy Cancelled** 🚫\n**task**: ${this.task}\n**A**: ${this.copyfromformat}\n**B**: ${this.copytoformat}\n**time elapsed**: ${this.time}\n**data copied**: ${this.datacopied}`;
-	};
+		this.path = `\`${this.copyfrom} > ${this.copyto}\``;
+		this.copyfromformat = `\`${this.copyfrom}\``;
+		this.copytoformat = `\`${this.copyto}\``;
+
+		if (this.status == 'Success') {
+			this.message = `**Copy Successful** ✅\n**task**: ${this.task}\n**A**: ${this.copyfromformat}\n**B**: ${this.copytoformat}\n**time elapsed**: ${this.time}\n**data copied**: ${this.datacopied}`;
+		} else 
+		if (this.status == 'Cancelled') {
+			this.message = `**Copy Cancelled** 🚫\n**task**: ${this.task}\n**A**: ${this.copyfromformat}\n**B**: ${this.copytoformat}\n**time elapsed**: ${this.time}\n**data copied**: ${this.datacopied}`;
+		};
+	}
 
 } // END CCCContext
 
--- test.js
+++ test.js
@@ -5,12 +5,14 @@ var discord = new ContextBuilder.DiscordContext()
 var twilio = new ContextBuilder.TwilioContext()
 var github = new ContextBuilder.GithubContext()
 var frameio = new ContextBuilder.FrameioContext()
+var ccc = new ContextBuilder.CCCContext({result: 'Editing Projects LOCAL BACKUP|Macintosh ' +'HD/Users/ryanfarber/Editing ' +'LOCAL|/Volumes/Backups/BACKUP_Editing ' +'Local|8/26/20, 5:52 PM|0:02|Zero KB|Cancelled|0'})
 
 console.log(twitch)
 console.log(discord)
 console.log(twilio)
 console.log(github)
 console.log(frameio)
+console.log(ccc)
 
 
 
