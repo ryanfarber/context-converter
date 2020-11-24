bed1a9b14bffcb6b15868bc84358c14acb1c318c
ryan farber <32393930+ryanfarber@users.noreply.github.com>
2020-11-23 23:21:39 -0800

    added isMentioned feature to twitch context

--- contexts/twitch-context.js
+++ contexts/twitch-context.js
@@ -1,12 +1,13 @@
 // twitch-context.js
 
 
-function TwitchContext(channel, userstate = {}, message, settings) {
+function TwitchContext(channel, userstate = {}, message, botUsername) {
   // if (!channel || !userstate || !message) console.warn("TWITCHCONTEXT: check arguments (channel, userstate, message)")
-  
+
   this.name = 'twitch';
   this.type = userstate["message-type"];
   this.timestamp = userstate['tmi-sent-ts'];
+  this.isMentioned = checkIfMentioned(message, botUsername);
   this.user = {
     name: userstate.username,
     id: userstate['user-id'],
@@ -22,5 +23,16 @@ function TwitchContext(channel, userstate = {}, message, settings) {
     id: userstate["room-id"],
   };
 };
+
+function checkIfMentioned(message, botUsername) {
+  if (!botUsername) return undefined
+  botUsername = (botUsername.startsWith("@")) ? botUsername : `@${botUsername}`   // checks if inputted username string starts with an @, if not it adds one
+  let messageArgs = message.split(" ")  // split message into arguments
+  if (messageArgs.includes(botUsername)) {
+    return true
+  } else {
+    return false
+  }
+}
 
 module.exports = TwitchContext
