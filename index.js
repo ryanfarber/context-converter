911df3c0e2b6f88616045a04d16f4f1eb5ca6500
Ryan Farber <32393930+ryanfarber@users.noreply.github.com>
2021-01-31 20:19:50 -0800

    update

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-// index.js
+// module index.js
 
-module.exports = {
-	CCCContext: require("./contexts/ccc-context.js"),
-	DiscordContext: require("./contexts/discord-context.js"),
-	FrameioContext: require("./contexts/frameio-context.js"),
-	TrelloContext: require("./contexts/trello-context.js"),
-	TwilioContext: require("./contexts/twilio-context.js"),
-	TwitchContext: require("./contexts/twitch-context.js"),
-	TelegramContext: require("./contexts/telegram-context.js"),
-	TransmissionContext: require("./contexts/transmission-context.js"),
-	GithubContext: require("./contexts/github-context.js"),
-	WebContext: require("./contexts/web-context.js"),
-	TwilioErrorContext: require("./contexts/twilio-error-context.js")
-}
-
-module.exports.v2 = require("./src")
+module.exports = require("./src");
+module.exports.v1 = require("./v1");
+module.exports.v2 = require("./src");
---
+++ v1/index.js
@@ -0,0 +1,15 @@
+
+
+module.exports = {
+	CCCContext: require("./ccc-context.js"),
+	DiscordContext: require("./discord-context.js"),
+	FrameioContext: require("./frameio-context.js"),
+	TrelloContext: require("./trello-context.js"),
+	TwilioContext: require("./twilio-context.js"),
+	TwitchContext: require("./twitch-context.js"),
+	TelegramContext: require("./telegram-context.js"),
+	TransmissionContext: require("./transmission-context.js"),
+	GithubContext: require("./github-context.js"),
+	WebContext: require("./web-context.js"),
+	TwilioErrorContext: require("./twilio-error-context.js")
+}
